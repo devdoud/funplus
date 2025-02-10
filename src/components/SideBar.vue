e442279b1823cc22e8b0f9f8c657d2e8e811cdbb
devdoud <bouraimawadoud@gmail.com>
2025-02-10 14:46:50 +0100

    add tailwind css

---
+++ src/components/SideBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-red-500">
+    This is a side bar component
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
